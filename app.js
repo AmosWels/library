7c42b58a9a2195cd65c40003de1d1af0408fe7c7
Amos Welike <amoswelike@gmail.com>
2019-04-04 18:37:47 +0300

    refactoring in vscode with eslint

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var path = require('path');
-var morgan = require('morgan')  
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const path = require("path");
+const morgan = require("morgan");
 
-var app = express();
-app.use(morgan('tiny'));
-app.use(express.static( path.join(__dirname, '/public/')))
-app.use('/css', express.static(path.join(__dirname, '/noe_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/noe_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, '/noe_modules/jquery/dist ')))
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, '/views/index.html')) ;
-})
+const app = express();
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/noe_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/noe_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/noe_modules/jquery/dist "))
+);
+app.get("/", (req, res)=> {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
 
-app.listen(3000,function(){
-    debug("listening to port " + chalk.green('3000')); 
+app.listen(3000, ()=> {
+  debug("listening to port " + chalk.green("3000"));
 });
