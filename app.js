9db5c900bdc75224cb0988229351ab5203ad63ff
Amos Welike <amoswelike@gmail.com>
2019-04-04 20:53:57 +0300

    rendering pages with routes and data from datastructures

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ const morgan = require("morgan");
 
 const port = process.env.PORT || 3000
 const app = express();
+const bookRouter = express.Router();
+
+const books = [
+  {
+    title: 'War and peace',
+    genre: 'Historical fiction',
+    author: 'Amos welike',
+    read: false
+  },
+  {
+    title: 'Les and Mani',
+    genre: 'Historical fiction',
+    author: 'Mbabazi Charity',
+    read: false
+  },
+  {
+    title: 'Gen lee warn dernene',
+    genre: 'Historical fiction',
+    author: 'Kasumba steven',
+    read: false
+  }
+]
+
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public/")));
 app.use(
@@ -24,6 +47,21 @@ app.use(
 app.set('views','./src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+.get((req, res)=>{
+  res.render('books',
+  {
+     nav:[{link:'/books',title:'Books'},{link:'/authors',title:'Authors'}], 
+     title:'Library',
+     books
+    });
+})
+bookRouter.route('/single')
+.get((req, res)=>{
+  res.send('hello Single book')
+})
+
+app.use('/books',  bookRouter);
 app.get("/", (req, res)=> {
   res.render('index',
    {
