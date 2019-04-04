bc43201d991cf5aa2964abbd1297d84aebde0200
Amos Welike <amoswelike@gmail.com>
2019-04-04 17:00:37 +0300

    serving index.html page

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Clone the repo
 #### Run `npm install`
 #### Run `node app.js`
+#### Run `DEBUG=app node app.js` for Debug mode
--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
+var path = require('path');
 var morgan = require('morgan')  
 
 var app = express();
 app.use(morgan('tiny'));
 app.get('/', function(req,res){
-    res.send('Hello from my library app');
+    res.sendFile(path.join(__dirname, '/views/index.html')) ;
 })
 
 app.listen(3000,function(){
     debug("listening to port " + chalk.green('3000')); 
-    // console.log(`${chalk.green('listening to port 3000')}`); 
-    // debug(`${chalk.green('listening to port 3000')}`); 
 });
